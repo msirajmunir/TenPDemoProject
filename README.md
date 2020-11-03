90719988760965a6d5fd970f73f2af46194e13b7
Siraj Munir <xirajmunir@gmail.com>
2020-11-03 13:22:07 +0500

    Added code comments and Readme file.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # TenPDemoProject
  This is Page Object Model project written in JAVA using TestNG, Selenide, Maven & WebDriver. 
+ 
+ ##Prerequisites:
+ The following items needs to be installed in your machine to run this project:
+ 1. Java Development Kit 1.8
+ 2. Maven 3.6.0
+ 3. IntelliJ Idea 2020 (IDE)
+ 
+ ##To run the test suite with Maven Command:
+ 
+ 1. Open the project directory.
+ 2. Now open the Power Shell on that directory.
+ 3. Type the following commands:
+ ```bash
+ mvn clean test -Dsurefire
+```
+It will open up the Chrome browser and executes the entire Smoke Test Suite. 
--- src/test/java/TestCases/SmokeTestCases.java
+++ src/test/java/TestCases/SmokeTestCases.java
@@ -17,20 +17,21 @@ import static com.codeborne.selenide.Selenide.open;
 
 
 public class SmokeTestCases extends ConnectionString {
+    //Creating a object of Page classes.
     HomePage homePageObj;
     Chapter1Page chapter1PageObj;
 
     @Test(priority = 0)
     public void openTheWebsite(){
        // driver.get("http://book.theautomatedtester.co.uk/"); //Selenium way of opening the URL
-        open("http://book.theautomatedtester.co.uk/"); //Selenide Way
+        open("http://book.theautomatedtester.co.uk/"); //Selenide Way of opening the URL
         System.out.println("Home Page Title: " + driver.getTitle());
     }
 
     @Test (priority = 1)
     public void TestCase01() {
         homePageObj = new HomePage();
-        homePageObj.clickOnChapter1();
+        homePageObj.clickOnChapter1(); //Calling function from HomePage class.
         System.out.println("Chapter1 Page Title: " + driver.getTitle());
        // Thread.sleep(2000);
 
@@ -39,9 +40,9 @@ public class SmokeTestCases extends ConnectionString {
     @Test(priority = 2)
     public void TestCase2(){
         chapter1PageObj = new Chapter1Page();
-        Assert.assertEquals(chapter1PageObj.assertSampleText1(),"Assert that this text is on the page");
-        System.out.println("Found this text: " + chapter1PageObj.assertSampleText1());
-        chapter1PageObj.clickOnHomePageLink();
+        Assert.assertEquals(chapter1PageObj.assertSampleText1(),"Assert that this text is on the page", "This will verify the text with the expected value. ");
+        System.out.println("Actual text: " + chapter1PageObj.assertSampleText1());
+        chapter1PageObj.clickOnHomePageLink(); //Calling function from ChapterPage class.
         System.out.println("Home Page Title: " + driver.getTitle());
     }
 }
